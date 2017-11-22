854b4bb34afa5deda13c76220b53ca31a35e2392
Rahul Deshpande <Rahul_AppDeveloper@outlook.com>
2017-11-22 03:01:29 -0800

    merge sort implementation complete!

--- merge_sort_gui.py
+++ merge_sort_gui.py
@@ -142,25 +142,76 @@ def merge_sorter(root, collection, canvas, kIndex, leftIndex, rightIndex, left_h
     global originalLength
     left_length = len(left_half)
     right_length = len(right_half)
+    print("collection = {}".format([element.value for element in collection]))
+    print("left = {}".format([element.value for element in left_half]))
+    print("right = {}".format([element.value for element in right_half]))
     print("in merger sorter inner method")
+    reset_colors(canvas, collection)
     if leftIndex < len(left_half) and rightIndex < len(right_half):
-        if left_half[leftIndex] < right_half[rightIndex]:
-            collection[kIndex] = left_half[leftIndex]
+        canvas.itemconfig(left_half[leftIndex].rectangle, fill='blue')        
+        canvas.itemconfig(right_half[rightIndex].rectangle, fill='blue')        
+        if left_half[leftIndex].value < right_half[rightIndex].value:  
+#            swap_xCoordinates(collection[kIndex], left_half[leftIndex])
+#            oldCoordinates = []
+#            oldCoordinates =  [left_half[leftIndex].coordinates[1], left_half[leftIndex].coordinates[3]]
+            collection[kIndex].value= left_half[leftIndex].value
+            collection[kIndex].coordinates[1], collection[kIndex].coordinates[3] =  left_half[leftIndex].coordinates[1], left_half[leftIndex].coordinates[3]
+#            collection[kIndex].coordinates[2], collection[kIndex].coordinates[3] =  left_half[leftIndex].coordinates[2], left_half[leftIndex].coordinates[3]  
+#            canvas.coords(left_half[leftIndex].rectangle,left_half[leftIndex].coordinates[0], left_half[leftIndex].coordinates[1],left_half[leftIndex].coordinates[2], left_half[leftIndex].coordinates[3])
+            canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])           
+
             leftIndex += 1
         else:
-            collection[kIndex] = right_half[rightIndex]
+            print("right is smaller than left")
+#            print("before change, left_half = {}".format([element.value for element in left_half]))
+#            oldCoordinates = []
+#            oldCoordinates =  [right_half[leftIndex].coordinates[2], right_half[leftIndex].coordinates[3]]
+#            swap_xCoordinates(collection[kIndex], right_half[rightIndex])
+#            canvas.coords(right_half[rightIndex].rectangle,right_half[rightIndex].coordinates[0], right_half[rightIndex].coordinates[1],right_half[rightIndex].coordinates[2], right_half[rightIndex].coordinates[3])
+            collection[kIndex].value = right_half[rightIndex].value
+            collection[kIndex].coordinates[1], collection[kIndex].coordinates[3] =  right_half[rightIndex].coordinates[1], right_half[rightIndex].coordinates[3]
+            canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])     
+#            print("after change, left_half = {}".format([element.value for element in left_half]))         
             rightIndex += 1
         kIndex += 1
+#        print("before calling merge sorter again,collection = {}".format([element.value for element in collection]))
+#        print("left = {}".format([element.value for element in left_half]))
+#        print("right = {}".format([element.value for element in right_half]))
         root.after(timeLapse, merge_sorter,  root, collection, canvas,  kIndex, leftIndex, rightIndex, left_half, right_half, callBackFunction, timeLapse)        
     elif leftIndex < left_length:
-        collection[kIndex] = left_half[leftIndex]
+        canvas.itemconfig(left_half[leftIndex].rectangle, fill='blue')   
+#        oldCoordinates = []
+#        oldCoordinates =  [left_half[leftIndex].coordinates[1], left_half[leftIndex].coordinates[3]]
+#        print("copy left over, ild y = {}".format(oldCoordinates))
+ #       swap_xCoordinates(collection[kIndex], left_half[leftIndex])
+ #       canvas.coords(left_half[leftIndex].rectangle,left_half[leftIndex].coordinates[0], left_half[leftIndex].coordinates[1],left_half[leftIndex].coordinates[2], left_half[leftIndex].coordinates[3])
+ #       canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])                
+        collection[kIndex].value = left_half[leftIndex].value
+        collection[kIndex].coordinates[1], collection[kIndex].coordinates[3] =  left_half[leftIndex].coordinates[1], left_half[leftIndex].coordinates[3]
+        #            canvas.coords(left_half[leftIndex].rectangle,left_half[leftIndex].coordinates[0], left_half[leftIndex].coordinates[1],left_half[leftIndex].coordinates[2], left_half[leftIndex].coordinates[3])
+        canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])           
         leftIndex += 1
         kIndex += 1
+#        print("before calling merge sorter again,collection = {}".format([element.value for element in collection]))
+#        print("left = {}".format([element.value for element in left_half]))
+#        print("right = {}".format([element.value for element in right_half]))
         root.after(timeLapse, merge_sorter,  root, collection, canvas,  kIndex, leftIndex, rightIndex, left_half, right_half, callBackFunction, timeLapse)        
     elif rightIndex < right_length:
-        collection[kIndex] = right_half[rightIndex]
+        print("copy right ober")
+        canvas.itemconfig(right_half[rightIndex].rectangle, fill='blue')        
+#        oldCoordinates = []
+#        oldCoordinates =  [right_half[leftIndex].coordinates[2], right_half[leftIndex].coordinates[3]]
+ #       swap_xCoordinates(collection[kIndex], right_half[rightIndex])
+ #       canvas.coords(right_half[rightIndex].rectangle,right_half[rightIndex].coordinates[0], right_half[rightIndex].coordinates[1],right_half[rightIndex].coordinates[2], right_half[rightIndex].coordinates[3])
+ #       canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])                
+        collection[kIndex].value = right_half[rightIndex].value
+        collection[kIndex].coordinates[1], collection[kIndex].coordinates[3] =right_half[rightIndex].coordinates[1], right_half[rightIndex].coordinates[3]
+        canvas.coords(collection[kIndex].rectangle,collection[kIndex].coordinates[0], collection[kIndex].coordinates[1], collection[kIndex].coordinates[2],  collection[kIndex].coordinates[3])                
         rightIndex += 1
         kIndex += 1
+#        print("before calling merge sorter again,collection = {}".format([element.value for element in collection]))
+#        print("left = {}".format([element.value for element in left_half]))
+#        print("right = {}".format([element.value for element in right_half]))
         root.after(timeLapse, merge_sorter,  root, collection, canvas,  kIndex, leftIndex, rightIndex, left_half, right_half, callBackFunction, timeLapse)
     else:
         # merging is complete.
@@ -174,15 +225,18 @@ def merge_outer(root, collection, canvas, startIndex,  callBackFunction, timeLap
         print("outer merge is complete, call the merge sorting callback registered.")
         callBackFunction(collection)
     else:
-        i = 0
-        j = 0
-        k = 0
+        leftIndex = 0
+        rightIndex = 0
+        kIndex = 0
         print("continue to divide the list")
         length = len(collection)
         midpoint = length // 2
-        left_half = collection[:midpoint]
-        right_half = collection[midpoint:]
-        merge_outer(root, left_half, canvas, startIndex, lambda data : merge_outer(root, right_half, canvas, startIndex + midpoint, lambda data1 : merge_sorter(root, collection, canvas, k, i, j, data, data1, callBackFunction, timeLapse), timeLapse))
+        left_half = copy.deepcopy(collection[:midpoint])
+        right_half = copy.deepcopy(collection[midpoint:])
+        print(id(collection[0]))
+        print(id(left_half[0]))
+
+        merge_outer(root, left_half, canvas, startIndex, lambda data : merge_outer(root, right_half, canvas, startIndex + midpoint, lambda data1 : merge_sorter(root, collection, canvas, kIndex, leftIndex, rightIndex, data, data1, callBackFunction, timeLapse), timeLapse))
         
 """
 
@@ -329,7 +383,7 @@ if __name__ == '__main__':
 
     unsorted_data = [float(item) for item in myData]
     unsorted_copy = copy.copy(unsorted_data)
-   
+    """
 #    unsorted_copy = reversed(list(range(1,100)))
 #    count = int(len(list(range(1,100))))
     count = len(unsorted_copy)
@@ -345,6 +399,37 @@ if __name__ == '__main__':
     for i in values:
         print(i)
 
+    """
+    count = len(unsorted_copy)
+    timeLapse = 100
+    width = window_width/int(count)
+    values = unsorted_copy
+    for item in values:
+        startx = (i * width)
+        starty = 0
+        dr = DataRectangle(item,'gray', [startx, starty], width, height)
+#        print("value = {}, coordinates = {}, id = {}".format(dr.value, dr.coordinates, dr.rectangle))
+        unsorted.append(dr)
+        i = i + 1
+        
+    #unsorted = [DataRectangle(item, 'red',[0,0], 50,50) for item in user_input.split(',')]
+    i = 0
+    for dr in unsorted:
+        print("value = {}. coordinates = {}".format(dr.value, dr.coordinates))
+    unsorted = assign_colors(unsorted)
+    unsorted = normalize_collection(unsorted)
+    unsorted = assign_heights(unsorted, 300)
+    draw_collection(unsorted, canvas)
+#    for dr in unsorted:
+#        print("value = {}, coordinates = {}, id = {}".format(dr.value, dr.coordinates, dr.rectangle))
+    
+#    print("start the loop")
+    #rectangle = canvas.create_rectangle(tuple([0,0, 100, 100]), fill ='red', outline = 'black')
+    #root.after(2000, lambda:move_rectangle(root, canvas, rectangle,[0,0, 100, 100]))
+    root.after(2000, lambda: merge_outer(root,  unsorted, canvas, 0, lambda x: [print("value = {}, coordinates = {}".format(element.value, element.coordinates)) for element in x], timeLapse))
+#    root.after(2000, insertion_sort, unsorted, canvas) 
+    root.mainloop()
+#    print(selection_sort(unsorted))
 
     root.mainloop()
 
